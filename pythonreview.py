b6245e11fc0d0c8d18fdf654b7b7e8f71c2c8b7d
maayansa24-meet <maayansapp@gmail.com>
2023-07-16 18:57:54 +0300

    Create pythonreview.py
    last task left

---
+++ pythonreview.py
@@ -0,0 +1,12 @@
+def create_youtube_video(titel,disription):
+	new_youtube_video = {"titel" : titel , "discription" : disription , likes : 0 , dislikes : 0 , comments{username : text} }
+
+def likes(youtube_video):
+	if likes in youtube_video():
+		youtube_video["likes"] += 1
+	reatern(youtube_video)
+
+def dilikes(youtube_video):
+	if likes in youtube_video():
+		youtube_video["dislikes"] += 1
+	reatern(youtube_video)
